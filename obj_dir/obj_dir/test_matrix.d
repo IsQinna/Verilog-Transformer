e0176c1ac5e50b97f814b2cf5ff2b40e164cf0b2
IsQinna <2790738157@qq.com>
2025-04-14 15:57:51 +0800

    update

--- obj_dir/obj_dir/test_matrix.d
+++
@@ -1,9 +0,0 @@
-test_matrix.o: ../test_matrix.cpp \
- /opt/verilator_path/share/verilator/include/verilated_vcd_c.h \
- /opt/verilator_path/share/verilator/include/verilated.h \
- /opt/verilator_path/share/verilator/include/verilatedos.h \
- /opt/verilator_path/share/verilator/include/verilated_config.h \
- /opt/verilator_path/share/verilator/include/verilated_types.h \
- /opt/verilator_path/share/verilator/include/verilated_funcs.h \
- /opt/verilator_path/share/verilator/include/verilated_trace.h \
- Vtransformer.h /opt/verilator_path/share/verilator/include/verilated.h
--- obj_dir/obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /opt/verilator_path/share/verilator/include/verilated_threads.cpp \
- /opt/verilator_path/share/verilator/include/verilatedos.h \
- /opt/verilator_path/share/verilator/include/verilated_threads.h \
- /opt/verilator_path/share/verilator/include/verilated.h \
- /opt/verilator_path/share/verilator/include/verilated_config.h \
- /opt/verilator_path/share/verilator/include/verilated_types.h \
- /opt/verilator_path/share/verilator/include/verilated_funcs.h
